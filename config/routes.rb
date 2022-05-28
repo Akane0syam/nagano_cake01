e0f3c37a16ce8a7b0cf2098ba9bc1caf5f32613a
Akane0syam <11kangae096@gmail.com>
2022-05-28 16:26:40 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  namespace :public do
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about', as: 'about'
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end

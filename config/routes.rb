91412d10e2cafb51efcb54d63cbad383322bd961
Akane0syam <11kangae096@gmail.com>
2022-05-31 14:37:45 +0000

    [Add]public、adminデータベース作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :customers
   devise_for :admins
 
-  namespace :public do
-    root to: 'homes#top'
-    get 'homes/about' => 'homes#about', as: 'about'
-
-    resources :items
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

7a69161c1430412f36d99d8c2d5e36e11f99fab4
Akane0syam <11kangae096@gmail.com>
2022-06-02 10:55:20 +0000

    [Add]db:migrateのエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -32,9 +32,7 @@ devise_for :customers,skip: [:passwords], controllers: {
 
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+
 
 
   namespace :public do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_141307) do
+ActiveRecord::Schema.define(version: 2022_06_02_104954) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false

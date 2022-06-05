726f1981485741a3fa119fe6d4576cfc30e49933
Akane0syam <11kangae096@gmail.com>
2022-06-05 13:07:14 +0000

    [Add]ジャンル編集画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+
+
   # admin
   # 管理者用
   # URL /admin/sign_in ...
@@ -8,19 +10,13 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
+    resources :genres,only: [:index,:create,:edit,:update]
+    get 'homes/top'
     get 'orders/show'
-  end
-  namespace :admin do
     get 'items/index'
     get 'items/new'
     get 'items/show'
     get 'items/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
     get 'customers/index'
     get 'customers/show'
     get 'customers/edit'
@@ -35,36 +31,21 @@ Rails.application.routes.draw do
   }
   namespace :public do
     get 'registrations/new'
-  end
-  namespace :public do
     get 'orders/new'
     get 'orders/confirm'
     get 'orders/complete'
     get 'orders/index'
     get 'orders/show'
-  end
-  namespace :public do
     get 'items/index'
     get 'items/show'
-  end
-  namespace :public do
     get 'homes/top'
     get 'homes/about'
-  end
-  namespace :public do
     get 'customers/show'
     get 'customers/edit'
     get 'customers/quit'
-  end
-
-  namespace :public do
     get 'cart_items/index'
-  end
-  namespace :public do
     get 'addresses/index'
     get 'addresses/edit'
   end
-
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
